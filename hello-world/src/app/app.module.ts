f9242942b4579a69a605829aba180991e6c60d1f
annam.manoj1996@gmail.com <manoj.annam@ggktech.com>
2019-05-17 18:49:32 +0530

    Usage of angular-font-awesome icons

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { CoursesComponent } from "./courses/courses.component";
 import { SummaryPipe } from "./pipes/summary.pipe";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
 
 @NgModule({
   declarations: [AppComponent, CoursesComponent, SummaryPipe],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AngularFontAwesomeModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
   entryComponents: [CoursesComponent]
