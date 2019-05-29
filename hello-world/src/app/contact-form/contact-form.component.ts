9913080fc92e5d8f641c656e457383d9aa30a591
ManojAnnam <annam.manoj1996@gmail.com>
2019-05-29 23:20:44 +0530

    Displaying how controls are created in Template-driven Forms

--- hello-world/src/app/contact-form/contact-form.component.ts
+++ hello-world/src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'contact-form',
-  templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.css']
+  selector: "contact-form",
+  templateUrl: "./contact-form.component.html",
+  styleUrls: ["./contact-form.component.css"]
 })
 export class ContactFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  log(x) {
+    console.log(x);
   }
-
 }
